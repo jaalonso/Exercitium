319c101f5b026236d7b772629658073d88d4a3b9
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-03-15 16:20:30 +0100

    Cuadrado_mas_cercano

--- src/Cuadrado_mas_cercano.hs
+++ src/Cuadrado_mas_cercano.hs
@@ -17,71 +17,85 @@
 
 module Cuadrado_mas_cercano where
 
-import Test.QuickCheck (Property, (==>), quickCheck)
+import Test.QuickCheck
 
 -- 1ª solución
 -- ===========
 
-cuadradoCercano :: Integer -> Integer
-cuadradoCercano n
-  | n - a^2 < (a+1)^2 - n = a^2
-  | otherwise             = (a+1)^2
-  where a = raizEntera n
+cuadradoCercano1 :: Integer -> Integer
+cuadradoCercano1 n
+  | n - b < c - n = b
+  | otherwise     = c
+  where a = raizEntera1 n
+        b = a^2
+        c = (a+1)^2
 
 -- (raizEntera x) es el mayor entero cuyo cuadrado no es mayor que
 -- x. Por ejemplo,
 --    raizEntera 8   ==  2
 --    raizEntera 9   ==  3
 --    raizEntera 10  ==  3
-raizEntera :: Integer -> Integer
-raizEntera x = aux (1,x)
-    where aux (a,b) | d == x    = c
-                    | c == a    = c
-                    | d < x     = aux (c,b)
-                    | otherwise = aux (a,c)
-              where c = (a+b) `div` 2
-                    d = c^2
+raizEntera1 :: Integer -> Integer
+raizEntera1 x =
+  last (takeWhile (\n -> n^2 <= x) [1..])
 
 -- 2ª solución
 -- ===========
 
 cuadradoCercano2 :: Integer -> Integer
 cuadradoCercano2 n
-  | n - a^2 < (a+1)^2 - n = a^2
-  | otherwise             = (a+1)^2
+  | n - b < c - n = b
+  | otherwise     = c
   where a = raizEntera2 n
+        b = a^2
+        c = (a+1)^2
 
--- (raizEntera2 x) es el mayor entero cuyo cuadrado no es mayor que
--- x. Por ejemplo,
---    raizEntera2 8   ==  2
---    raizEntera2 9   ==  3
---    raizEntera2 10  ==  3
 raizEntera2 :: Integer -> Integer
-raizEntera2 0 = 0
-raizEntera2 1 = 1
-raizEntera2 n = head (dropWhile supera (iterate mejora (div n 2)))
-  where mejora x = div (x + div n x) 2
-        supera x = x^2 > n
+raizEntera2 x = aux (1,x)
+    where aux (a,b) | d == x    = c
+                    | c == a    = c
+                    | x <= d    = aux (a,c)
+                    | otherwise = aux (c,b)
+            where c = (a+b) `div` 2
+                  d = c^2
 
 -- 3ª solución
 -- ===========
 
 cuadradoCercano3 :: Integer -> Integer
-cuadradoCercano3 = (^ 2) . round . sqrt . fromIntegral
+cuadradoCercano3 n
+  | n - b < c - n = b
+  | otherwise     = c
+  where a = raizEntera3 n
+        b = a^2
+        c = (a+1)^2
+
+raizEntera3 :: Integer -> Integer
+raizEntera3 0 = 0
+raizEntera3 1 = 1
+raizEntera3 n = until aceptable mejora n
+  where mejora x    = (x + n `div` x) `div` 2
+        aceptable x = x^2 <= n
+
+-- 4ª solución
+-- ===========
+
+cuadradoCercano4 :: Integer -> Integer
+cuadradoCercano4 = (^ 2) . round . sqrt . fromIntegral
 
 -- Equivalencia de las definiciones
 -- ================================
 
 -- La propiedad es
-prop_cuadradoCercano :: Integer -> Property
-prop_cuadradoCercano x =
-  x >= 0 ==> cuadradoCercano3 x == cuadradoCercano x
-
-verifica_cuadradoCercano :: IO ()
-verifica_cuadradoCercano = quickCheck prop_cuadradoCercano
+prop_cuadradoCercano :: Positive Integer -> Bool
+prop_cuadradoCercano (Positive x) =
+  all (== cuadradoCercano1 x)
+      [cuadradoCercano2 x,
+       cuadradoCercano3 x,
+       cuadradoCercano4 x]
 
 -- La comprobación es
---    λ> verifica_cuadradoCercano
+--    λ> quickCheck prop_cuadradoCercano
 --    +++ OK, passed 100 tests.
 
 -- Aunque ha pasado los 100 tests, las definiciones no son
@@ -92,3 +106,27 @@ verifica_cuadradoCercano = quickCheck prop_cuadradoCercano
 --    9999999999999998322278400000000070368744177664
 --    λ> cuadradoCercano (10^46)
 --    10000000000000000000000000000000000000000000000
+
+-- Comparación de eficiencia
+-- =========================
+
+-- La comparación es
+--    λ> cuadradoCercano1 (10^14)
+--    100000000000000
+--    (4.59 secs, 5,920,475,784 bytes)
+--    λ> cuadradoCercano2 (10^14)
+--    100000000000000
+--    (0.01 secs, 512,472 bytes)
+--    λ> cuadradoCercano3 (10^14)
+--    100000000000000
+--    (0.01 secs, 494,248 bytes)
+--    λ> cuadradoCercano4 (10^14)
+--    100000000000000
+--    (0.01 secs, 475,152 bytes)
+--
+--    λ> length (show (cuadradoCercano2 (10^20000)))
+--    20001
+--    (3.94 secs, 1,446,675,504 bytes)
+--    λ> length (show (cuadradoCercano3 (10^20000)))
+--    20001
+--    (4.50 secs, 926,647,904 bytes)
