02dbadcaf2c5ee19853210de3aef3ba58ff7727e
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-05 12:43:21 +0200

    Borrar 2ª solución en Media_aritmetica_de_tres_numeros_Spec.hs

--- test/Media_aritmetica_de_tres_numeros_Spec.hs
+++ test/Media_aritmetica_de_tres_numeros_Spec.hs
@@ -2,22 +2,15 @@ module Media_aritmetica_de_tres_numeros_Spec (main, spec) where
 
 import Media_aritmetica_de_tres_numeros
 import Test.Hspec
-import Test.QuickCheck
 
 main :: IO ()
 main = hspec spec
 
-specG :: (Float -> Float -> Float -> Float) -> Spec
-specG media3 = do
+spec :: Spec
+spec = do
   it "e1" $
     media3 1 3 8     `shouldBe`  4.0
   it "e2" $
     media3 (-1) 0 7  `shouldBe`  2.0
   it "e3" $
     media3 (-3) 0 3  `shouldBe`  0.0
-
-spec :: Spec
-spec = do
-  describe "def. 1" $ specG media3a
-  describe "def. 2" $ specG media3b
-  describe "equivalencia" $ it "p1" $ property prop_media3
