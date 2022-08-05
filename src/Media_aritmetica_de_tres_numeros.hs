d45ef28ad8ccc7a9ddb06eb39f8c3c66a8d8abf4
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-05 12:40:58 +0200

    Borrar 2ª solución en Media_aritmetica_de_tres_numeros.hs

--- src/Media_aritmetica_de_tres_numeros.hs
+++ src/Media_aritmetica_de_tres_numeros.hs
@@ -14,32 +14,7 @@
 --    media3 (-3) 0 3  ==  0.0
 -- ---------------------------------------------------------------------
 
-{-# OPTIONS_GHC -fno-warn-unused-imports #-}
-
 module Media_aritmetica_de_tres_numeros where
 
-import Test.QuickCheck
-
--- 1ª solución
--- ===========
-
-media3a :: Float -> Float -> Float -> Float
-media3a x y z = (x+y+z)/3
-
--- 2ª solución
--- ===========
-
-media3b :: Float -> Float -> Float -> Float
-media3b x y z = sum [x, y, z] / 3
-
--- Comprobación de equivalencia
--- ============================
-
--- La propiedad es
-prop_media3 :: Float -> Float -> Float -> Bool
-prop_media3 x y z =
-  media3a x y z == media3b x y z
-
--- La comprobación es
---    λ> quickCheck prop_media3
---    +++ OK, passed 100 tests.
+media3 :: Float -> Float -> Float -> Float
+media3 x y z = (x+y+z)/3
