d06b7bc138b1d25a691ed365c82523236174e88f
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-06 07:41:56 +0200

    Añadir 2ª solución a El_primero_al_final_Spec.hs

--- test/El_primero_al_final_Spec.hs
+++ test/El_primero_al_final_Spec.hs
@@ -6,7 +6,12 @@ import Test.Hspec
 main :: IO ()
 main = hspec spec
 
-spec :: Spec
-spec = do
+specG :: ([Int] -> [Int]) -> Spec
+specG rota1 = do
   it "e1" $
     rota1 [3,2,5,7]  `shouldBe`  [2,5,7,3]
+
+spec :: Spec
+spec = do
+  describe "def. 1" $ specG rota1a
+  describe "def. 2" $ specG rota1b
