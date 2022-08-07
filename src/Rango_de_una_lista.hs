0a616686699e298ebdd187b7cfd12c90087071c2
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-07 11:04:41 +0200

    Añadir enunciado a Rango_de_una_lista.hs.

---
+++ src/Rango_de_una_lista.hs
@@ -0,0 +1,13 @@
+-- Rango_de_una_lista.hs
+-- Rango de una lista.
+-- José A. Alonso Jiménez <https://jaalonso.github.io>
+-- Sevilla, 18-agosto-2022
+-- ---------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------
+-- Definir la función
+--    rango :: [Int] -> [Int]
+-- tal que (rango xs) es la lista formada por el menor y mayor elemento
+-- de xs.
+--    rango [3,2,7,5]  ==  [2,7]
+-- ---------------------------------------------------------------------
