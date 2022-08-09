663be16d3dd0e479863c5c438f8e537a8bfb3397
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-09 11:51:40 +0200

    Añadir 2ª definición a Elementos_finales_Spec.hs.

--- test/Elementos_finales_Spec.hs
+++ test/Elementos_finales_Spec.hs
@@ -7,6 +7,8 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec =
+spec = do
   it "e1" $
-    finales 3 [2,5,4,7,9,6]  `shouldBe`  [7,9,6]
+    finales1 3 [2,5,4,7,9,6]  `shouldBe`  [7,9,6]
+  it "e2" $
+    finales2 3 [2,5,4,7,9,6]  `shouldBe`  [7,9,6]
