110435f864e1cd264bcb981e846d70c5c6d2bf0e
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-16 19:11:43 +0200

    Añadir equivalencia de definiciones a Elementos_finales.hs.

--- src/Elementos_finales.hs
+++ src/Elementos_finales.hs
@@ -12,7 +12,10 @@
 --    finales 3 [2,5,4,7,9,6]  ==  [7,9,6]
 -- ---------------------------------------------------------------------
 
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
 module Elementos_finales where
+import Test.QuickCheck
 
 -- 1ª definición
 finales1 :: Int -> [a] -> [a]
@@ -21,3 +24,15 @@ finales1 n xs = drop (length xs - n) xs
 -- 2ª definición
 finales2 :: Int -> [a] -> [a]
 finales2 n xs = reverse (take n (reverse xs))
+
+-- Comprobación de equivalencia
+-- ============================
+
+-- La propiedad es
+prop_finales :: Int -> [Int] -> Bool
+prop_finales n xs =
+  finales1 n xs == finales2 n xs
+
+-- La comprobación es
+--    λ> quickCheck prop_finales
+--    +++ OK, passed 100 tests.
