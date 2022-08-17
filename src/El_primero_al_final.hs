6ed06e2f021cd5c0ed30c3f79dba80fb6752990c
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-17 11:17:03 +0200

    Añadir equivalencia a El_primero_al_final.hs.

--- src/El_primero_al_final.hs
+++ src/El_primero_al_final.hs
@@ -12,12 +12,17 @@
 --    rota1 [3,2,5,7]  ==  [2,5,7,3]
 -- ---------------------------------------------------------------------
 
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
 module El_primero_al_final where
+
+import Test.QuickCheck
 
 -- 1ª solución
 -- ===========
 
 rota1a :: [a] -> [a]
+rota1a [] = []
 rota1a xs = tail xs ++ [head xs]
 
 -- 2ª solución
@@ -26,3 +31,15 @@ rota1a xs = tail xs ++ [head xs]
 rota1b :: [a] -> [a]
 rota1b []     = []
 rota1b (x:xs) = xs ++ [x]
+
+-- Comprobación de equivalencia
+-- ============================
+
+-- La propiedad es
+prop_rota1 :: [Int] -> Bool
+prop_rota1 xs =
+  rota1a xs == rota1b xs
+
+-- La comprobación es
+--    λ> quickCheck prop_rota1
+--    +++ OK, passed 100 tests.
