1f2d5f3f24ed689a4bcca6c5d9aadc4fffaed9b6
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-23 12:31:31 +0200

    Reescribir test/Numeros_perfectos_Spec.hs

--- test/Numeros_perfectos_Spec.hs
+++ test/Numeros_perfectos_Spec.hs
@@ -16,9 +16,4 @@ spec :: Spec
 spec = do
   describe "def. 1" $ specG perfectos1
   describe "def. 2" $ specG perfectos2
-  describe "def. 3" $ specG perfectos3
-  describe "def. 4" $ specG perfectos4
-  describe "def. 5" $ specG perfectos5
-  describe "def. 6" $ specG perfectos6
-  describe "def. 7" $ specG perfectos7
   describe "equivalencia" $ it "p1" $ property prop_perfectos
