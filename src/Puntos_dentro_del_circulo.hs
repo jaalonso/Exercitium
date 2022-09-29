0593d54c22f325844757db8c4462b2e754c923bb
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-29 11:28:18 +0200

    Ampliar enunciado de src/Puntos_dentro_del_circulo.hs

--- src/Puntos_dentro_del_circulo.hs
+++ src/Puntos_dentro_del_circulo.hs
@@ -5,14 +5,21 @@
 -- ---------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------
+-- En el círculo de radio 2 hay 6 puntos cuyas coordenadas son puntos
+-- naturales:
+--    (0,0),(0,1),(0,2),(1,0),(1,1),(2,0)
+-- y en de radio 3 hay 11:
+--    (0,0),(0,1),(0,2),(0,3),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2),(3,0)
+--
 -- Definir la función
 --    circulo :: Int -> Int
 -- tal que (circulo n) es el la cantidad de pares de números naturales
--- (x,y) que se encuentran dentro del círculo de radio n. Por ejemplo,
---    circulo 3  ==  9
---    circulo 4  ==  15
---    circulo 5  ==  22
---    circulo 100  ==  7949
+-- (x,y) que se encuentran en el círculo de radio n. Por ejemplo,
+--    circulo 1    ==  3
+--    circulo 2    ==  6
+--    circulo 3    ==  11
+--    circulo 4    ==  17
+--    circulo 100  ==  7955
 -- ---------------------------------------------------------------------
 
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
@@ -25,22 +32,25 @@ import Test.QuickCheck
 -- ===========
 
 circulo1 :: Int -> Int
-circulo1 n = length [(x,y) | x <- [0..n], y <- [0..n],
-                             x*x+y*y < n*n]
+circulo1 n = length (enCirculo1 n)
+
+enCirculo1 :: Int -> [(Int, Int)]
+enCirculo1 n = [(x,y) | x <- [0..n],
+                        y <- [0..n],
+                        x*x+y*y <= n*n]
 
 -- 2ª solución
 -- ===========
 
 circulo2 :: Int -> Int
-circulo2 n = length [(x,y) | x <- [0..n-1]
-                           , y <- [0..raizCuadradaEntera (n*n - x*x)]
-                           , x*x+y*y < n*n]
+circulo2 0 = 1
+circulo2 n =
+  2 * length (enSemiCirculo n) + ceiling(fromIntegral n / sqrt 2)
 
--- (raizCuadradaEntera n) es la parte entera de la raíz cuadrada de
--- n. Por ejemplo,
---    raizCuadradaEntera 17  ==  4
-raizCuadradaEntera :: Int -> Int
-raizCuadradaEntera n = truncate (sqrt (fromIntegral n))
+enSemiCirculo :: Int -> [(Int, Int)]
+enSemiCirculo n =
+  [(x,y) | x <- [0..floor (sqrt (fromIntegral (n * n)))],
+           y <- [x+1..truncate (sqrt (fromIntegral (n*n - x*x)))]]
 
 -- Comprobación de equivalencia
 -- ============================
@@ -59,8 +69,8 @@ prop_circulo (Positive n) =
 
 -- La comparación es
 --    λ> circulo1 (2*10^3)
---    3143579
---    (4.51 secs, 1,744,161,832 bytes)
+--    3143587
+--    (3.58 secs, 1,744,162,600 bytes)
 --    λ> circulo2 (2*10^3)
---    3143579
---    (2.85 secs, 1,435,776,200 bytes)
+--    3143587
+--    (0.41 secs, 266,374,208 bytes)
