3aef40412ff18f7851428c93e88aa7c9a1d044f4
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-12-30 13:55:31 +0100

    Número de sumas en una expresión aritmética

---
+++ src/Numero_de_sumas_en_una_expresion_aritmetica.hs
@@ -0,0 +1,37 @@
+-- Numero_de_sumas_en_una_expresion_aritmetica.hs
+-- Número de sumas en una expresión aritmética.
+-- José A. Alonso Jiménez <https://jaalonso.github.io>
+-- Sevilla, 12-enero-2023
+-- ---------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------
+-- Las expresiones aritméticas con variables pueden representarse usando
+-- el siguiente tipo de datos
+--    data Expr = C Int
+--              | V Char
+--              | S Expr Expr
+--              | P Expr Expr
+-- Por ejemplo, la expresión 2*(a+5) se representa por
+--    P (C 2) (S (V 'a') (C 5))
+--
+-- Definir la función
+--    sumas :: Expr -> Int
+-- tal que (sumas e) es el número de sumas en la expresión e. Por
+-- ejemplo,
+--    sumas (P (V 'z') (S (C 3) (V 'x')))  ==  1
+--    sumas (S (V 'z') (S (C 3) (V 'x')))  ==  2
+--    sumas (P (V 'z') (P (C 3) (V 'x')))  ==  0
+-- ---------------------------------------------------------------------
+
+module Numero_de_sumas_en_una_expresion_aritmetica where
+
+data Expr = C Int
+          | V Char
+          | S Expr Expr
+          | P Expr Expr
+
+sumas :: Expr -> Int
+sumas (V _)   = 0
+sumas (C _)   = 0
+sumas (S x y) = 1 + sumas x + sumas y
+sumas (P x y) = sumas x + sumas y
---
+++ test/Numero_de_sumas_en_una_expresion_aritmetica_Spec.hs
@@ -0,0 +1,16 @@
+module Numero_de_sumas_en_una_expresion_aritmetica_Spec (main, spec) where
+
+import Numero_de_sumas_en_una_expresion_aritmetica
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  it "e1" $
+    sumas (P (V 'z') (S (C 3) (V 'x')))  `shouldBe`  1
+  it "e2" $
+    sumas (S (V 'z') (S (C 3) (V 'x')))  `shouldBe`  2
+  it "e3" $
+    sumas (P (V 'z') (P (C 3) (V 'x')))  `shouldBe`  0
