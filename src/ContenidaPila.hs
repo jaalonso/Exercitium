439541e5dc9202a39c5b81c40817b9f4e75aed5d
Jose A. Alonso <JoseA.Alonso@gmail.com>
2023-01-11 17:44:26 +0100

    Inclusión de pilas.

---
+++ src/ContenidaPila.hs
@@ -0,0 +1,81 @@
+-- ContenidaPila.hs
+-- Inclusión de pilas.
+-- José A. Alonso Jiménez <https://jaalonso.github.io>
+-- Sevilla, 27-enero-2023
+-- ---------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------
+-- Definir la función
+--    contenidaPila :: Eq a => Pila a -> Pila a -> Bool
+-- tal que (contenidaPila p1 p2) se verifica si todos los elementos de
+-- de la pila p1 son elementos de la pila p2. Por ejemplo,
+--    λ> ej1 = apila 3 (apila 2 vacia)
+--    λ> ej2 = apila 3 (apila 4 vacia)
+--    λ> ej3 = apila 5 (apila 2 (apila 3 vacia))
+--    λ> contenidaPila ej1 ej3
+--    True
+--    λ> contenidaPila ej2 ej3
+--    False
+-- ---------------------------------------------------------------------
+
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+module ContenidaPila where
+
+import TAD.PilaConListas
+import Test.QuickCheck
+
+-- 1ª solución
+-- ===========
+
+contenidaPila1 :: Eq a => Pila a -> Pila a -> Bool
+contenidaPila1 p1 p2
+  | esVacia p1 = True
+  | otherwise  = pertenecePila cp1 p2 && contenidaPila1 dp1 p2
+  where cp1 = cima p1
+        dp1 = desapila p1
+
+-- (pertenecePila y p) se verifica si y es un elemento de la pila p. Por
+-- ejemplo,
+--    pertenecePila 2 (apila 5 (apila 2 (apila 3 vacia))) == True
+--    pertenecePila 4 (apila 5 (apila 2 (apila 3 vacia))) == False
+pertenecePila :: Eq a => a -> Pila a -> Bool
+pertenecePila x p
+  | esVacia p  = False
+  | otherwise  = x == cp || pertenecePila x dp
+  where cp = cima p
+        dp = desapila p
+
+-- 2ª solución
+-- ===========
+
+contenidaPila2 :: Eq a => Pila a -> Pila a -> Bool
+contenidaPila2 p1 p2 =
+  contenidaLista (pilaAlista p1) (pilaAlista p2)
+
+contenidaLista :: Eq a => [a] -> [a] -> Bool
+contenidaLista xs ys =
+  all (`elem` ys) xs
+
+-- (pilaALista p) es la lista formada por los elementos de la
+-- lista p. Por ejemplo,
+--    λ> pilaAlista (apila 5 (apila 2 (apila 3 vacia)))
+--    [3, 2, 5]
+pilaAlista :: Pila a -> [a]
+pilaAlista = reverse . aux
+  where aux p | esVacia p = []
+              | otherwise = cp : aux dp
+          where cp = cima p
+                dp = desapila p
+
+-- Comprobación de equivalencia
+-- ============================
+
+-- La propiedad es
+prop_contenidaPila :: Pila Int -> Pila Int -> Bool
+prop_contenidaPila p1 p2 =
+  contenidaPila1 p1 p2 == contenidaPila2 p1 p2
+
+-- La comprobación es
+--    λ> quickCheck prop_contenidaPila
+--    +++ OK, passed 100 tests.
---
+++ test/ContenidaPila_Spec.hs
@@ -0,0 +1,25 @@
+module ContenidaPila_Spec (main, spec) where
+
+import ContenidaPila
+import TAD.PilaConListas
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+specG :: (Pila Int -> Pila Int -> Bool) -> Spec
+specG contenidaPila = do
+  it "e1" $
+    contenidaPila ej1 ej3 `shouldBe` True
+  it "e2" $
+    contenidaPila ej2 ej3 `shouldBe` False
+  where
+    ej1 = apila 3 (apila 2 vacia)
+    ej2 = apila 3 (apila 4 vacia)
+    ej3 = apila 5 (apila 2 (apila 3 vacia))
+
+
+spec :: Spec
+spec = do
+  describe "def. 1" $ specG contenidaPila1
+  describe "def. 2" $ specG contenidaPila2
