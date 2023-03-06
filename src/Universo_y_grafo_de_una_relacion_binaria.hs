fd3c24c7f53ed701597dbbea0e16af8efe9bfdbf
Jose A. Alonso <JoseA.Alonso@gmail.com>
2023-03-06 12:35:18 +0100

    Universo y grafo de una relación binaria.

--- src/Universo_y_grafo_de_una_relacion_binaria.hs
+++ src/Universo_y_grafo_de_una_relacion_binaria.hs
@@ -1,34 +1,30 @@
 -- Universo_y_grafo_de_una_relacion_binaria.hs
 -- Universo y grafo de una relación binaria.
 -- José A. Alonso Jiménez <https://jaalonso.github.io>
--- Sevilla, 28-marzo-2023
+-- Sevilla, 29-marzo-2023
 -- ---------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------
--- Una relación binaria R sobre un conjunto A se puede representar
--- mediante un par (xs,ps) donde xs es la lista de los elementos de A
--- (el universo de R) y ps es la lista de pares de R (el grafo de R).
---
--- Definir el tipo de dato (Rel a), para representar las relaciones
--- binarias sobre a, y las siguientes funciones
+-- Usando el [tipo de las relaciones binarias](https://bit.ly/3IVVqOT),
+-- definir las siguientes funciones
 --    universo :: Eq a => Rel a -> [a]
 --    grafo    :: Eq a => ([a],[(a,a)]) -> [(a,a)]
 -- tales que
 -- + (universo r) es el universo de la relación r. Por ejemplo,
---      λ> r = ([1..9],[(1,3),(2,6),(8,9),(2,7)])
+--      λ> r = R ([1, 3],[(3, 1), (3, 3)])
 --      λ> universo r
---      [1,2,3,4,5,6,7,8,9]
+--      [1,3]
 -- + (grafo r) es el grafo de la relación r. Por ejemplo,
 --      λ> grafo r
---      [(1,3),(2,6),(8,9),(2,7)]
+--      [(3,1),(3,3)]
 -- ---------------------------------------------------------------------
 
 module Universo_y_grafo_de_una_relacion_binaria where
 
-type Rel a = ([a],[(a,a)])
+import Relaciones_binarias
 
 universo :: Eq a => Rel a -> [a]
-universo (us,_) = us
+universo (R (u,_)) = u
 
 grafo :: Eq a => Rel a -> [(a,a)]
-grafo (_,ps) = ps
+grafo (R (_,g)) = g
--- test/Universo_y_grafo_de_una_relacion_binaria_Spec.hs
+++ test/Universo_y_grafo_de_una_relacion_binaria_Spec.hs
@@ -1,6 +1,7 @@
 module Universo_y_grafo_de_una_relacion_binaria_Spec (main, spec) where
 
 import Universo_y_grafo_de_una_relacion_binaria
+import Relaciones_binarias
 import Test.Hspec
 
 main :: IO ()
@@ -9,8 +10,8 @@ main = hspec spec
 spec :: Spec
 spec = do
   it "e1" $
-    universo r `shouldBe` [1,2,3,4,5,6,7,8,9]
+    universo r `shouldBe` [1,3]
   it "e2" $
-    grafo r `shouldBe` [(1,3),(2,6),(8,9),(2,7)]
+    grafo r `shouldBe` [(3, 1), (3, 3)]
   where
-    r = ([1..9],[(1,3),(2,6),(8,9),(2,7)])
+    r = R ([1, 3],[(3, 1), (3, 3)])
