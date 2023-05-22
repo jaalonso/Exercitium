461642cbd6f8e54d42c8b695768045dab564cd6e
Jose A. Alonso <JoseA.Alonso@gmail.com>
2023-05-22 13:27:08 +0200

    Grafos completos.

--- src/Grafo_Grafos_completos.hs
+++ src/Grafo_Grafos_completos.hs
@@ -1,5 +1,5 @@
 -- Grafo_Grafos_completos.hs
--- Grafos completos.
+-- TAD de los grafos: Grafos_completos.
 -- José A. Alonso Jiménez <https://jaalonso.github.io>
 -- Sevilla, 15-mayo-2023
 -- ---------------------------------------------------------------------
@@ -20,7 +20,28 @@
 module Grafo_Grafos_completos where
 
 import TAD.Grafo (Grafo, Orientacion (ND), creaGrafo')
+import Test.Hspec (Spec, hspec, it, shouldBe)
 
 completo :: Int -> Grafo Int Int
 completo n =
   creaGrafo' ND (1,n) [(x,y) | x <- [1..n], y <- [x+1..n]]
+
+-- Verificación
+-- ============
+
+verifica :: IO ()
+verifica = hspec spec
+
+spec :: Spec
+spec = do
+  it "e1" $
+    show (completo 4) `shouldBe`
+    "G ND ([1,2,3,4],[(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)])"
+
+-- La verificación es
+--    λ> verifica
+--
+--    e1
+--
+--    Finished in 0.0004 seconds
+--    1 example, 0 failures
--- test/Grafo_Grafos_completos_Spec.hs
+++ test/Grafo_Grafos_completos_Spec.hs
@@ -5,9 +5,3 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec spec
-
-spec :: Spec
-spec = do
-  it "e1" $
-    show (completo 4) `shouldBe`
-    "G ND ([1,2,3,4],[(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)])"
