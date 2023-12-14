f9bbfd34485c646b6483a1000e2d6dc228b45dcf
Jose A. Alonso <JoseA.Alonso@gmail.com>
2023-12-14 17:37:17 +0100

    Números de Pentanacci

--- src/Numeros_de_Pentanacci.hs
+++ src/Numeros_de_Pentanacci.hs
@@ -23,16 +23,20 @@
 -- Los primeros números de Pentanacci son
 --   0, 1, 1, 2, 4, 8, 16, 31, 61, 120, 236, 464, 912, 1793, 3525, ...
 --
--- Definir la sucesión
---    pentanacci :: [Integer]
--- cuyos elementos son los números de Pentanacci. Por ejemplo,
---    λ> take 15 pentanacci
---    [0,1,1,2,4,8,16,31,61,120,236,464,912,1793,3525]
---    λ> (pentanacci !! (10^5)) `mod` (10^30)
---    482929150584077921552549215816
---    231437922897686901289110700696
---    λ> length (show (pentanacci !! (10^5)))
---    29357
+-- Definir las funciones
+--    pentanacci  :: Integer -> Integer
+--    pentanaccis :: [Integer]
+-- tales que
+-- + (pentanacci n) es el n-ésimo número de Pentanacci. Por ejemplo,
+--      λ> pentanacci 14
+--      3525
+--      λ> pentanacci (10^5) `mod` 10^30
+--      482929150584077921552549215816
+--      λ> length (show (pentanacci (10^5)))
+--      29357
+-- + pentanaccis es la lista de los números de Pentanacci. Por ejemplo,
+--      λ> take 15 pentanacci
+--      [0,1,1,2,4,8,16,31,61,120,236,464,912,1793,3525]
 -- ---------------------------------------------------------------------
 
 {-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
@@ -40,46 +44,55 @@
 
 module Numeros_de_Pentanacci where
 
-import Data.List (zipWith5)
+import Data.List (genericIndex, zipWith5)
 import Test.Hspec (Spec, hspec, it, shouldBe)
 import Test.QuickCheck (NonNegative (NonNegative), quickCheckWith, maxSize, stdArgs)
 
 -- 1ª solución
 -- ===========
 
-pentanacci1 :: [Integer]
-pentanacci1 = [pent n | n <- [0..]]
+pentanacci1  :: Integer -> Integer
+pentanacci1  0 = 0
+pentanacci1  1 = 1
+pentanacci1  2 = 1
+pentanacci1  3 = 2
+pentanacci1  4 = 4
+pentanacci1  n = sum [pentanacci1 (n-k) | k <- [1..5]]
 
-pent :: Integer -> Integer
-pent 0 = 0
-pent 1 = 1
-pent 2 = 1
-pent 3 = 2
-pent 4 = 4
-pent n = pent (n-1) + pent (n-2) + pent (n-3) + pent (n-4) + pent (n-5)
+pentanaccis1 :: [Integer]
+pentanaccis1 = [pentanacci1 n | n <- [0..]]
 
 -- 2ª solución
 -- ===========
 
-pentanacci2 :: [Integer]
-pentanacci2 =
+pentanaccis2 :: [Integer]
+pentanaccis2 =
   0 : 1 : 1 : 2 : 4 : zipWith5 f (r 0) (r 1) (r 2) (r 3) (r 4)
   where f a b c d e = a+b+c+d+e
-        r n         = drop n pentanacci2
+        r n         = drop n pentanaccis2
+
+pentanacci2  :: Integer -> Integer
+pentanacci2 n = pentanaccis2 `genericIndex` n
 
 -- 3ª solución
 -- ===========
 
-pentanacci3 :: [Integer]
-pentanacci3 = p (0, 1, 1, 2, 4)
+pentanaccis3 :: [Integer]
+pentanaccis3 = p (0, 1, 1, 2, 4)
   where p (a, b, c, d, e) = a : p (b, c, d, e, a + b + c + d + e)
+
+pentanacci3  :: Integer -> Integer
+pentanacci3 n = pentanaccis3 `genericIndex` n
 
 -- 4ª solución
 -- ===========
 
-pentanacci4 :: [Integer]
-pentanacci4 = 0: 1: 1: 2: 4: p pentanacci4
+pentanaccis4 :: [Integer]
+pentanaccis4 = 0: 1: 1: 2: 4: p pentanaccis4
   where p (a:b:c:d:e:xs) = (a+b+c+d+e): p (b:c:d:e:xs)
+
+pentanacci4  :: Integer -> Integer
+pentanacci4 n = pentanaccis4 `genericIndex` n
 
 -- Verificación
 -- ============
@@ -90,16 +103,16 @@ verifica = hspec spec
 spec :: Spec
 spec = do
   it "ej1" $
-    take 15 pentanacci1 `shouldBe`
+    take 15 pentanaccis1 `shouldBe`
     [0,1,1,2,4,8,16,31,61,120,236,464,912,1793,3525]
   it "ej2" $
-    take 15 pentanacci2 `shouldBe`
+    take 15 pentanaccis2 `shouldBe`
     [0,1,1,2,4,8,16,31,61,120,236,464,912,1793,3525]
   it "ej3" $
-    take 15 pentanacci3 `shouldBe`
+    take 15 pentanaccis3 `shouldBe`
     [0,1,1,2,4,8,16,31,61,120,236,464,912,1793,3525]
   it "ej4" $
-    take 15 pentanacci4 `shouldBe`
+    take 15 pentanaccis4 `shouldBe`
     [0,1,1,2,4,8,16,31,61,120,236,464,912,1793,3525]
 
 -- La verificación es
@@ -111,37 +124,37 @@ spec = do
 -- ============================
 
 -- La propiedad es
-prop_pentanacci :: NonNegative Int -> Bool
-prop_pentanacci (NonNegative n) =
-  all (== pentanacci1 !! n)
-      [pentanacci1 !! n,
-       pentanacci2 !! n,
-       pentanacci3 !! n]
+prop_pentanaccis :: NonNegative Int -> Bool
+prop_pentanaccis (NonNegative n) =
+  all (== pentanaccis1 !! n)
+      [pentanaccis1 !! n,
+       pentanaccis2 !! n,
+       pentanaccis3 !! n]
 
 -- La comprobación es
---    λ> quickCheckWith (stdArgs {maxSize=25}) prop_pentanacci
+--    λ> quickCheckWith (stdArgs {maxSize=25}) prop_pentanaccis
 --    +++ OK, passed 100 tests.
 
 -- Comparación de eficiencia
 -- =========================
 
 -- La comparación es
---    λ> pentanacci1 !! 25
+--    λ> pentanacci1 25
 --    5976577
---    (3.18 secs, 1,025,263,896 bytes)
---    λ> pentanacci2 !! 25
+--    (4.64 secs, 1,865,626,496 bytes)
+--    λ> pentanacci2 25
 --    5976577
---    (0.00 secs, 562,360 bytes)
+--    (0.00 secs, 578,584 bytes)
 --
---    λ> length (show (pentanacci2 !! (10^5)))
+--    λ> length (show (pentanacci2 (10^5)))
 --    29357
---    (1.04 secs, 2,531,259,408 bytes)
---    λ> length (show (pentanacci3 !! (10^5)))
+--    (1.16 secs, 2,543,272,136 bytes)
+--    λ> length (show (pentanacci3 (10^5)))
 --    29357
---    (1.00 secs, 2,548,868,384 bytes)
---    λ> length (show (pentanacci4 !! (10^5)))
+--    (1.00 secs, 2,560,881,608 bytes)
+--    λ> length (show (pentanacci4 (10^5)))
 --    29357
---    (0.96 secs, 2,580,065,520 bytes)
+--    (1.03 secs, 2,592,078,744 bytes)
 
 -- Referencias
 -- ===========
