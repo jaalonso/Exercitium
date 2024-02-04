981ee20b6c85377b5d5b100337c5c100408af2ba
Jose A. Alonso <JoseA.Alonso@gmail.com>
2024-02-04 08:00:49 +0100

    La función indicatriz de Euler

--- src/La_funcion_indicatriz_de_Euler.hs
+++ src/La_funcion_indicatriz_de_Euler.hs
@@ -1,7 +1,7 @@
 -- La_funcion_indicatriz_de_Euler.hs
 -- La función indicatriz de Euler.
 -- José A. Alonso Jiménez <https://jaalonso.github.io>
--- Sevilla, 22-julio-2022
+-- Sevilla, 24-enero-2024
 -- ---------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------
@@ -11,9 +11,9 @@
 -- número de enteros positivos menores o iguales a n y coprimos con
 -- n. Por ejemplo, φ(36) = 12 ya que los números menores o iguales a 36
 -- y coprimos con 36 son doce: 1, 5, 7, 11, 13, 17, 19, 23, 25, 29, 31,
--- y 35.   
--- 
--- Definir la función 
+-- y 35.
+--
+-- Definir la función
 --    phi :: Integer -> Integer
 -- tal que (phi n) es igual a φ(n). Por ejemplo,
 --    phi 36                          ==  12
@@ -22,7 +22,7 @@
 --    length (show (phi (10^(10^5)))) == 100000
 --
 -- Comprobar con QuickCheck que, para todo n > 0, φ(10^n) tiene n
--- dígitos. 
+-- dígitos.
 -- ---------------------------------------------------------------------
 
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
@@ -33,19 +33,20 @@ import Data.List (genericLength, group)
 import Data.Numbers.Primes (primeFactors)
 import Math.NumberTheory.ArithmeticFunctions (totient)
 import Test.QuickCheck (Positive (Positive), quickCheck)
+import Test.Hspec (Spec, describe, hspec, it, shouldBe)
 
 -- 1ª solución
 -- ===========
 
 phi1 :: Integer -> Integer
-phi1 n = genericLength [x | x <- [1..n], gcd x n == 1] 
+phi1 n = genericLength [x | x <- [1..n], gcd x n == 1]
 
 -- 2ª solución
 -- ===========
 
 phi2 :: Integer -> Integer
-phi2 n = product [(p-1)*p^(e-1) | (p,e) <- factorizacion n] 
-    
+phi2 n = product [(p-1)*p^(e-1) | (p,e) <- factorizacion n]
+
 factorizacion :: Integer -> [(Integer,Integer)]
 factorizacion n =
   [(head xs,genericLength xs) | xs <- group (primeFactors n)]
@@ -54,20 +55,45 @@ factorizacion n =
 -- =============
 
 phi3 :: Integer -> Integer
-phi3 n = 
+phi3 n =
   product [(x-1) * product xs | (x:xs) <- group (primeFactors n)]
 
 -- 4ª solución
 -- =============
 
 phi4 :: Integer -> Integer
-phi4 = totient 
+phi4 = totient
+
+-- Verificación
+-- ============
+
+verifica :: IO ()
+verifica = hspec spec
+
+specG :: (Integer -> Integer) -> Spec
+specG phi = do
+  it "e1" $
+    phi 36 `shouldBe` 12
+  it "e2" $
+    map phi [10..20] `shouldBe` [4,10,4,12,6,8,8,16,6,18,8]
+
+spec :: Spec
+spec = do
+  describe "def. 1" $ specG phi1
+  describe "def. 2" $ specG phi2
+  describe "def. 3" $ specG phi3
+  describe "def. 4" $ specG phi4
+
+-- La verificación es
+--    λ> verifica
+--
+--    8 examples, 0 failures
 
 -- Comprobación de equivalencia
 -- ============================
 
 -- La propiedad es
-prop_phi :: Positive Integer -> Bool 
+prop_phi :: Positive Integer -> Bool
 prop_phi (Positive n) =
   all (== phi1 n)
       [phi2 n,
@@ -88,7 +114,7 @@ prop_phi (Positive n) =
 --    λ> phi2 (2*10^6)
 --    800000
 --    (0.02 secs, 565,664 bytes)
---    
+--
 --    λ> length (show (phi2 (10^100000)))
 --    100000
 --    (2.80 secs, 5,110,043,208 bytes)
--- test/La_funcion_indicatriz_de_Euler_Spec.hs
+++ test/La_funcion_indicatriz_de_Euler_Spec.hs
@@ -1,21 +1,7 @@
 module La_funcion_indicatriz_de_Euler_Spec (main, spec) where
 
-import La_funcion_indicatriz_de_Euler
-import Test.Hspec
-import Test.QuickCheck
+import La_funcion_indicatriz_de_Euler (spec)
+import Test.Hspec (hspec)
 
 main :: IO ()
 main = hspec spec
-
-specG :: (Integer -> Integer) -> Spec
-specG phi = do
-  it "e1" $
-    map phi [10..20] `shouldBe` [4,10,4,12,6,8,8,16,6,18,8]
-
-spec :: Spec
-spec = do
-  describe "def. 1" $ specG phi1
-  describe "def. 2" $ specG phi2
-  describe "def. 3" $ specG phi3
-  describe "def. 4" $ specG phi4
-  describe "equivalencia" $ it "p1" $ property prop_phi
