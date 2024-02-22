9c5949be8cf54c5fc17c805f08341b7147e15cd7
Jose A. Alonso <JoseA.Alonso@gmail.com>
2024-02-22 18:53:13 +0100

    Sistema factorádico de numeración.

--- src/Sistema_factoradico_de_numeracion.hs
+++ src/Sistema_factoradico_de_numeracion.hs
@@ -1,7 +1,7 @@
 -- Sistema_factoradico_de_numeracion.hs
 -- Sistema factorádico de numeración.
--- José A. Alonso Jiménez
--- Sevilla, 28-enero-2022
+-- José A. Alonso Jiménez <https://jaalonso.github.io>
+-- Sevilla, 19-febrero-2024
 -- ---------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------
@@ -25,18 +25,6 @@
 -- tales que
 -- + (factoradicoAdecimal cs) es el número decimal correspondiente al
 --   número factorádico cs. Por ejemplo,
---      λ> decimalAfactoradico 463
---      "341010"
---      λ> decimalAfactoradico 2022
---      "2441000"
---      λ> decimalAfactoradico 36288000
---      "A0000000000"
---      λ> map decimalAfactoradico [1..10]
---      ["10","100","110","200","210","1000","1010","1100","1110","1200"]
---      λ> decimalAfactoradico 37199332678990121746799944815083519999999
---      "3KXWVUTSRQPONMLKJIHGFEDCBA9876543210"
--- + (decimalAfactoradico n) es el número factorádico correpondiente al
---   número decimal n. Por ejemplo,
 --      λ> factoradicoAdecimal "341010"
 --      463
 --      λ> factoradicoAdecimal "2441000"
@@ -47,6 +35,18 @@
 --      [1,2,3,4,5,6,7,8,9,10]
 --      λ> factoradicoAdecimal "3KXWVUTSRQPONMLKJIHGFEDCBA9876543210"
 --      37199332678990121746799944815083519999999
+-- + (decimalAfactoradico n) es el número factorádico correpondiente al
+--   número decimal n. Por ejemplo,
+--      λ> decimalAfactoradico 463
+--      "341010"
+--      λ> decimalAfactoradico 2022
+--      "2441000"
+--      λ> decimalAfactoradico 36288000
+--      "A0000000000"
+--      λ> map decimalAfactoradico [1..10]
+--      ["10","100","110","200","210","1000","1010","1100","1110","1200"]
+--      λ> decimalAfactoradico 37199332678990121746799944815083519999999
+--      "3KXWVUTSRQPONMLKJIHGFEDCBA9876543210"
 --
 -- Comprobar con QuickCheck que, para cualquier entero positivo n,
 --    factoradicoAdecimal (decimalAfactoradico n) == n
@@ -59,10 +59,11 @@ module Sistema_factoradico_de_numeracion where
 
 import Data.List (genericIndex, genericLength)
 import qualified Data.Map as M ((!), Map, fromList)
-import Test.QuickCheck (Property, (==>), quickCheck)
+import Test.Hspec (Spec, describe, hspec, it, shouldBe)
+import Test.QuickCheck
 
--- 1ª solución
--- ===========
+-- 1ª definición de factoradicoAdecimal
+-- ====================================
 
 factoradicoAdecimal1 :: String -> Integer
 factoradicoAdecimal1 cs = sum (zipWith (*) xs ys)
@@ -93,24 +94,8 @@ caracteres = ['0'..'9'] ++ ['A'..'Z']
 facts :: [Integer]
 facts = scanl (*) 1 [1..]
 
-decimalAfactoradico1 :: Integer -> String
-decimalAfactoradico1 n = aux n (reverse (takeWhile (<=n) facts))
-  where aux 0 xs     = ['0' | _ <- xs]
-        aux m (x:xs) = enteroAcaracter (m `div` x) : aux (m `mod` x) xs
-
--- (enteroAcaracter k) es el k-ésimo elemento de la lista
--- ['0', '1',..., '9', 'A', 'B',..., 'Z']. . Por ejemplo,
---    enteroAcaracter 0   ==  '0'
---    enteroAcaracter 1   ==  '1'
---    enteroAcaracter 9   ==  '9'
---    enteroAcaracter 10  ==  'A'
---    enteroAcaracter 11  ==  'B'
---    enteroAcaracter 35  ==  'Z'
-enteroAcaracter :: Integer -> Char
-enteroAcaracter k = caracteres `genericIndex` k
-
--- 2ª solución
--- ===========
+-- 2ª definición de factoradicoAdecimal
+-- ====================================
 
 factoradicoAdecimal2 :: String -> Integer
 factoradicoAdecimal2 cs = sum (zipWith (*) xs ys)
@@ -134,6 +119,66 @@ caracterAentero2 c = caracteresEnteros M.! c
 caracteresEnteros :: M.Map Char Integer
 caracteresEnteros = M.fromList (zip (['0'..'9'] ++ ['A'..'Z']) [0..])
 
+-- 3ª definición de factoradicoAdecimal
+-- ====================================
+
+factoradicoAdecimal3 :: String -> Integer
+factoradicoAdecimal3 cs =
+  sum (zipWith (*) facts (reverse (map caracterAentero3 cs)))
+
+-- (caracterAentero3 c) es la posición del carácter c en la lista de
+-- caracteres ['0', '1',..., '9', 'A', 'B',..., 'Z']. Por ejemplo,
+--    caracterAentero3 '0'  ==  0
+--    caracterAentero3 '1'  ==  1
+--    caracterAentero3 '9'  ==  9
+--    caracterAentero3 'A'  ==  10
+--    caracterAentero3 'B'  ==  11
+--    caracterAentero3 'Z'  ==  35
+caracterAentero3 :: Char -> Integer
+caracterAentero3 c =
+  genericLength (takeWhile (/= c) caracteres)
+
+-- 4ª definición de factoradicoAdecimal
+-- ====================================
+
+factoradicoAdecimal4 :: String -> Integer
+factoradicoAdecimal4 =
+  sum . zipWith (*) facts . reverse . map caracterAentero4
+
+-- (caracterAentero4 c) es la posición del carácter c en la lista de
+-- caracteres ['0', '1',..., '9', 'A', 'B',..., 'Z']. Por ejemplo,
+--    caracterAentero4 '0'  ==  0
+--    caracterAentero4 '1'  ==  1
+--    caracterAentero4 '9'  ==  9
+--    caracterAentero4 'A'  ==  10
+--    caracterAentero4 'B'  ==  11
+--    caracterAentero4 'Z'  ==  35
+caracterAentero4 :: Char -> Integer
+caracterAentero4 =
+  genericLength . flip takeWhile caracteres . (/=)
+
+-- 1ª definición de decimalAfactoradico
+-- ====================================
+
+decimalAfactoradico1 :: Integer -> String
+decimalAfactoradico1 n = aux n (reverse (takeWhile (<=n) facts))
+  where aux 0 xs     = ['0' | _ <- xs]
+        aux m (x:xs) = enteroAcaracter (m `div` x) : aux (m `mod` x) xs
+
+-- (enteroAcaracter k) es el k-ésimo elemento de la lista
+-- ['0', '1',..., '9', 'A', 'B',..., 'Z']. . Por ejemplo,
+--    enteroAcaracter 0   ==  '0'
+--    enteroAcaracter 1   ==  '1'
+--    enteroAcaracter 9   ==  '9'
+--    enteroAcaracter 10  ==  'A'
+--    enteroAcaracter 11  ==  'B'
+--    enteroAcaracter 35  ==  'Z'
+enteroAcaracter :: Integer -> Char
+enteroAcaracter k = caracteres `genericIndex` k
+
+-- 2ª definición de decimalAfactoradico
+-- ====================================
+
 decimalAfactoradico2 :: Integer -> String
 decimalAfactoradico2 n = aux n (reverse (takeWhile (<=n) facts))
   where aux 0 xs     = ['0' | _ <- xs]
@@ -155,24 +200,8 @@ enteroAcaracter2 k = enterosCaracteres M.! k
 enterosCaracteres :: M.Map Integer Char
 enterosCaracteres = M.fromList (zip [0..] caracteres)
 
--- 3ª solución
--- ===========
-
-factoradicoAdecimal3 :: String -> Integer
-factoradicoAdecimal3 cs =
-  sum (zipWith (*) facts (reverse (map caracterAentero3 cs)))
-
--- (caracterAentero3 c) es la posición del carácter c en la lista de
--- caracteres ['0', '1',..., '9', 'A', 'B',..., 'Z']. Por ejemplo,
---    caracterAentero3 '0'  ==  0
---    caracterAentero3 '1'  ==  1
---    caracterAentero3 '9'  ==  9
---    caracterAentero3 'A'  ==  10
---    caracterAentero3 'B'  ==  11
---    caracterAentero3 'Z'  ==  35
-caracterAentero3 :: Char -> Integer
-caracterAentero3 c =
-  genericLength (takeWhile (/= c) caracteres)
+-- 3ª definición de decimalAfactoradico
+-- ====================================
 
 decimalAfactoradico3 :: Integer -> String
 decimalAfactoradico3 n = aux "" 2 (n, 0)
@@ -191,24 +220,8 @@ enteroAcaracter3 :: Integer -> Char
 enteroAcaracter3 n =
   caracteres !! fromInteger n
 
--- 4ª solución
--- ===========
-
-factoradicoAdecimal4 :: String -> Integer
-factoradicoAdecimal4 =
-  sum . zipWith (*) facts . reverse . map caracterAentero4
-
--- (caracterAentero4 c) es la posición del carácter c en la lista de
--- caracteres ['0', '1',..., '9', 'A', 'B',..., 'Z']. Por ejemplo,
---    caracterAentero4 '0'  ==  0
---    caracterAentero4 '1'  ==  1
---    caracterAentero4 '9'  ==  9
---    caracterAentero4 'A'  ==  10
---    caracterAentero4 'B'  ==  11
---    caracterAentero4 'Z'  ==  35
-caracterAentero4 :: Char -> Integer
-caracterAentero4 =
-  genericLength . flip takeWhile caracteres . (/=)
+-- 4ª definición de decimalAfactoradico
+-- ====================================
 
 decimalAfactoradico4 :: Integer -> String
 decimalAfactoradico4 = f "" 2 . (, 0)
@@ -226,6 +239,46 @@ decimalAfactoradico4 = f "" 2 . (, 0)
 enteroAcaracter4 :: Integer -> Char
 enteroAcaracter4 = (caracteres `genericIndex`)
 
+-- Verificación
+-- ============
+
+verifica :: IO ()
+verifica = hspec spec
+
+specG1 :: (String -> Integer) -> Spec
+specG1 factoradicoAdecimal = do
+  it "e1" $
+    factoradicoAdecimal "341010" `shouldBe` 463
+  it "e2" $
+    factoradicoAdecimal "2441000" `shouldBe` 2022
+  it "e3" $
+    factoradicoAdecimal "A0000000000" `shouldBe` 36288000
+
+specG2 :: (Integer -> String) -> Spec
+specG2 decimalAfactoradico = do
+  it "e1" $
+    decimalAfactoradico 463 `shouldBe` "341010"
+  it "e2" $
+    decimalAfactoradico 2022 `shouldBe` "2441000"
+  it "e3" $
+    decimalAfactoradico 36288000 `shouldBe` "A0000000000"
+
+spec :: Spec
+spec = do
+  describe "def. 1" $ specG1 factoradicoAdecimal1
+  describe "def. 2" $ specG1 factoradicoAdecimal2
+  describe "def. 3" $ specG1 factoradicoAdecimal3
+  describe "def. 4" $ specG1 factoradicoAdecimal4
+  describe "def. 1" $ specG2 decimalAfactoradico1
+  describe "def. 2" $ specG2 decimalAfactoradico2
+  describe "def. 3" $ specG2 decimalAfactoradico3
+  describe "def. 4" $ specG2 decimalAfactoradico4
+
+-- La verificación es
+--    λ> verifica
+--
+--    24 examples, 0 failures
+
 -- Propiedad de inverso
 -- ====================
 
@@ -237,12 +290,9 @@ prop_factoradico n =
   factoradicoAdecimal3 (decimalAfactoradico3 n) == n &&
   factoradicoAdecimal4 (decimalAfactoradico4 n) == n
 
-verifica_prop_factoradico :: IO ()
-verifica_prop_factoradico = quickCheck prop_factoradico
-
 -- La comprobación es
---    λ> verifica_prop_factoradico
---    +++ OK, passed 100 tests.
+--    λ> quickCheck prop_factoradico
+--    +++ OK, passed 100 tests; 101 discarded.
 
 -- Comparación de eficiencia
 -- =========================
--- test/Sistema_factoradico_de_numeracion_Spec.hs
+++ test/Sistema_factoradico_de_numeracion_Spec.hs
@@ -1,29 +1,7 @@
 module Sistema_factoradico_de_numeracion_Spec (main, spec) where
 
-import Sistema_factoradico_de_numeracion
-import Test.Hspec
+import Sistema_factoradico_de_numeracion (spec)
+import Test.Hspec (hspec)
 
 main :: IO ()
 main = hspec spec
-
-specG :: (String -> Integer) -> (Integer -> String) -> Spec
-specG factoradicoAdecimal decimalAfactoradico = do
-  it "e1" $
-    decimalAfactoradico 463      `shouldBe` "341010"
-  it "e2" $
-    decimalAfactoradico 8999     `shouldBe` "15243210"
-  it "e3" $
-    decimalAfactoradico 2982     `shouldBe` "4041000"
-  it "e4" $
-    decimalAfactoradico 36288000 `shouldBe` "A0000000000"
-  it "e5" $
-    factoradicoAdecimal "341010" `shouldBe` 463
-  it "e6" $
-    factoradicoAdecimal "15243210" `shouldBe` 8999
-
-spec :: Spec
-spec = do
-  describe "def. 1" $ specG factoradicoAdecimal1 decimalAfactoradico1
-  describe "def. 2" $ specG factoradicoAdecimal2 decimalAfactoradico2
-  describe "def. 3" $ specG factoradicoAdecimal3 decimalAfactoradico3
-  describe "def. 4" $ specG factoradicoAdecimal4 decimalAfactoradico4
