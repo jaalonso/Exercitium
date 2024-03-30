95326fa7f078c24056f55fb9f087dcd1cbf90b5a
Jose A. Alonso <JoseA.Alonso@gmail.com>
2024-03-30 13:30:03 +0100

    Exponente en la factorización.

--- src/Exponente_en_la_factorizacion.hs
+++ src/Exponente_en_la_factorizacion.hs
@@ -1,14 +1,14 @@
 -- Exponente_en_la_factorizacion.hs
--- Exponente en la factorización
--- José A. Alonso Jiménez
--- Sevilla, 8-febrero-2022
+-- Exponente en la factorización.
+-- José A. Alonso Jiménez <https://jaalonso.github.io>
+-- Sevilla, 19-marzo-24
 -- ---------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------
 -- Definir la función
 --    exponente :: Integer -> Integer -> Int
--- tal que (exponente x n) es el exponente de x en la factorizacón prima
--- de n (se supone que x > 1 y n > 0). Por ejemplo,
+-- tal que (exponente x n) es el exponente de x en la factorización
+-- prima de n (se supone que x > 1 y n > 0). Por ejemplo,
 --    exponente 2 24  ==  3
 --    exponente 3 24  ==  1
 --    exponente 6 24  ==  0
@@ -18,6 +18,7 @@
 module Exponente_en_la_factorizacion where
 
 import Data.Numbers.Primes (primeFactors)
+import Test.Hspec (Spec, describe, hspec, it, shouldBe)
 import Test.QuickCheck
 
 -- 1ª solución
@@ -45,7 +46,7 @@ exponente2 x n
   | esPrimo x = length (takeWhile (`divisible` x) (iterate (`div` x) n))
   | otherwise = 0
 
--- (divisible n x) se verifica si ne divisible por x. Por ejemplo,
+-- (divisible n x) se verifica si n es divisible por x. Por ejemplo,
 --    divisible 6 2  ==  True
 --    divisible 7 2  ==  False
 divisible :: Integer -> Integer -> Bool
@@ -57,6 +58,34 @@ divisible n x = n `mod` x == 0
 exponente3 :: Integer -> Integer -> Int
 exponente3 x n =
   length (filter (==x) (primeFactors n))
+
+-- Verificación
+-- ============
+
+verifica :: IO ()
+verifica = hspec spec
+
+specG :: (Integer -> Integer -> Int) -> Spec
+specG exponente = do
+  it "e1" $
+    exponente 2 24  `shouldBe`  3
+  it "e2" $
+    exponente 3 24  `shouldBe`  1
+  it "e3" $
+    exponente 6 24  `shouldBe`  0
+  it "e4" $
+    exponente 7 24  `shouldBe`  0
+
+spec :: Spec
+spec = do
+  describe "def. 1" $ specG exponente1
+  describe "def. 2" $ specG exponente2
+  describe "def. 3" $ specG exponente3
+
+-- La verificación es
+--    λ> verifica
+--
+--    12 examples, 0 failures
 
 -- Equivalencia de las definiciones
 -- ================================
