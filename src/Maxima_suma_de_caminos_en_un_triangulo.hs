221031b4de32e3e87807ee47ea42529a941a4c08
Jose A. Alonso <JoseA.Alonso@gmail.com>
2025-01-10 14:17:03 +0100

    Actualiza "Máxima suma de caminos en un triángulo"

--- src/Maxima_suma_de_caminos_en_un_triangulo.hs
+++ src/Maxima_suma_de_caminos_en_un_triangulo.hs
@@ -1,7 +1,7 @@
 -- Maxima_suma_de_caminos_en_un_triangulo.hs
 -- Máxima suma de caminos en un triángulo.
 -- José A. Alonso Jiménez <https://jaalonso.github.io>
--- Sevilla, 16-abril-2024
+-- Sevilla, 10-enero-2025
 -- ---------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------
