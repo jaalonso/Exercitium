62bf8761d761f093bec0b79c116e28955b4e2530
Jose A. Alonso <JoseA.Alonso@gmail.com>
2025-04-02 14:28:33 +0200

    Añade: Mayor producto de las ramas de un árbol

---
+++ test/A2014/M05/Mayor_producto_de_las_ramas_de_un_arbol_Spec.hs
@@ -0,0 +1,7 @@
+module A2014.M05.Mayor_producto_de_las_ramas_de_un_arbol_Spec (main, spec) where
+
+import A2014.M05.Mayor_producto_de_las_ramas_de_un_arbol (spec)
+import Test.Hspec (hspec)
+
+main :: IO ()
+main = hspec spec
