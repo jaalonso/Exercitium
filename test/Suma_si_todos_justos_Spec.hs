cc7bd2902cf45346c24d32fe8dfab020dca51980
Jose A. Alonso <JoseA.Alonso@gmail.com>
2025-08-25 18:39:49 +0200

    Actualiza "Suma si todos los valores son justos"

--- test/Suma_si_todos_justos_Spec.hs
+++ test/Suma_si_todos_justos_Spec.hs
@@ -1,24 +1,7 @@
 module Suma_si_todos_justos_Spec (main, spec) where
 
-import Suma_si_todos_justos
-import Test.Hspec
+import Suma_si_todos_justos (spec)
+import Test.Hspec (hspec)
 
 main :: IO ()
 main = hspec spec
-
-specG :: ([Maybe Integer] -> Maybe Integer) -> Spec
-specG sumaSiTodosJustos = do
-  it "e1" $
-    sumaSiTodosJustos [Just 2, Just 5]           `shouldBe` Just 7
-  it "e2" $
-    sumaSiTodosJustos [Just 2, Just 5, Nothing]  `shouldBe` Nothing
-
-spec :: Spec
-spec = do
-  describe "def. 1" $ specG sumaSiTodosJustos1
-  describe "def. 2" $ specG sumaSiTodosJustos2
-  describe "def. 3" $ specG sumaSiTodosJustos3
-  describe "def. 4" $ specG sumaSiTodosJustos4
-  describe "def. 5" $ specG sumaSiTodosJustos5
-  describe "def. 6" $ specG sumaSiTodosJustos6
-  describe "def. 7" $ specG sumaSiTodosJustos7
