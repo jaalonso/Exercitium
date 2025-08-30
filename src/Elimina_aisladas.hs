eb7cfbf07117fe904ae2518c56b9ac8491769e82
Jose A. Alonso <JoseA.Alonso@gmail.com>
2025-08-30 18:15:14 +0200

    Actualiza "Eliminación de las ocurrencias aisladas"

--- src/Elimina_aisladas.hs
+++ src/Elimina_aisladas.hs
@@ -1,7 +1,7 @@
 -- Elimina_aisladas.hs
 -- Eliminación de las ocurrencias aisladas.
 -- José A. Alonso Jiménez <https://jaalonso.github.io>
--- Sevilla, 20-abril-2022
+-- Sevilla, 11-Junio-2014 (actualizado 30-Agosto-2025)
 -- ---------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------
@@ -36,6 +36,7 @@
 module Elimina_aisladas where
 
 import Data.List (group)
+import Test.Hspec (Spec, describe, hspec, it, shouldBe)
 import Test.QuickCheck (quickCheck)
 
 -- 1ª solución
@@ -67,14 +68,71 @@ eliminaAisladas2 x ys
 -- ===========
 
 eliminaAisladas3 :: Eq a => a -> [a] -> [a]
-eliminaAisladas3 x ys = concat [zs | zs <- group ys, zs /= [x]]
+eliminaAisladas3 x ys =
+  concat [zs | zs <- group ys, zs /= [x]]
 
 -- 4ª solución
 -- ===========
 
 eliminaAisladas4 :: Eq a => a -> [a] -> [a]
-eliminaAisladas4 x = concat . filter (/= [x]) . group
+eliminaAisladas4 x =
+  concat . filter (/= [x]) . group
 
+-- Verificación
+-- ============
+
+verifica :: IO ()
+verifica = hspec spec
+
+specG :: (Char -> String -> String) -> Spec
+specG eliminaAisladas = do
+  it "e1" $
+    eliminaAisladas 'X' ""                `shouldBe` ""
+  it "e2" $
+    eliminaAisladas 'X' "X"               `shouldBe` ""
+  it "e3" $
+    eliminaAisladas 'X' "XX"              `shouldBe` "XX"
+  it "e4" $
+    eliminaAisladas 'X' "XXX"             `shouldBe` "XXX"
+  it "e5" $
+    eliminaAisladas 'X' "abcd"            `shouldBe` "abcd"
+  it "e6" $
+    eliminaAisladas 'X' "Xabcd"           `shouldBe` "abcd"
+  it "e7" $
+    eliminaAisladas 'X' "XXabcd"          `shouldBe` "XXabcd"
+  it "e8" $
+    eliminaAisladas 'X' "XXXabcd"         `shouldBe` "XXXabcd"
+  it "e9" $
+    eliminaAisladas 'X' "abcdX"           `shouldBe` "abcd"
+  it "e10" $
+    eliminaAisladas 'X' "abcdXX"          `shouldBe` "abcdXX"
+  it "e11" $
+    eliminaAisladas 'X' "abcdXXX"         `shouldBe` "abcdXXX"
+  it "e12" $
+    eliminaAisladas 'X' "abXcd"           `shouldBe` "abcd"
+  it "e13" $
+    eliminaAisladas 'X' "abXXcd"          `shouldBe` "abXXcd"
+  it "e14" $
+    eliminaAisladas 'X' "abXXXcd"         `shouldBe` "abXXXcd"
+  it "e15" $
+    eliminaAisladas 'X' "XabXcdX"         `shouldBe` "abcd"
+  it "e16" $
+    eliminaAisladas 'X' "XXabXXcdXX"      `shouldBe` "XXabXXcdXX"
+  it "e17" $
+    eliminaAisladas 'X' "XXXabXXXcdXXX"   `shouldBe` "XXXabXXXcdXXX"
+  it "e18" $
+    eliminaAisladas 'X' "XabXXcdXeXXXfXx" `shouldBe` "abXXcdeXXXfx"
+
+spec :: Spec
+spec = do
+  describe "def. 1"  $ specG eliminaAisladas1
+  describe "def. 2"  $ specG eliminaAisladas2
+  describe "def. 3"  $ specG eliminaAisladas3
+  describe "def. 4"  $ specG eliminaAisladas4
+
+-- La verificación es
+--    λ> verifica
+--    72 examples, 0 failures
 
 -- Comprobación de equivalencia
 -- ============================
--- test/Elimina_aisladas_Spec.hs
+++ test/Elimina_aisladas_Spec.hs
@@ -1,55 +1,7 @@
 module Elimina_aisladas_Spec (main, spec) where
 
-import Elimina_aisladas
-import Test.Hspec
-import Test.QuickCheck
+import Elimina_aisladas (spec)
+import Test.Hspec (hspec)
 
 main :: IO ()
 main = hspec spec
-
-specG :: (Char -> String -> String) -> Spec
-specG eliminaAisladas = do
-  it "e1" $
-    eliminaAisladas 'X' ""                  `shouldBe` ""
-  it "e2" $
-    eliminaAisladas 'X' "X"                 `shouldBe` ""
-  it "e3" $
-    eliminaAisladas 'X' "XX"                `shouldBe` "XX"
-  it "e4" $
-    eliminaAisladas 'X' "XXX"               `shouldBe` "XXX"
-  it "e5" $
-    eliminaAisladas 'X' "abcd"              `shouldBe` "abcd"
-  it "e6" $
-    eliminaAisladas 'X' "Xabcd"             `shouldBe` "abcd"
-  it "e7" $
-    eliminaAisladas 'X' "XXabcd"            `shouldBe` "XXabcd"
-  it "e8" $
-    eliminaAisladas 'X' "XXXabcd"           `shouldBe` "XXXabcd"
-  it "e9" $
-    eliminaAisladas 'X' "abcdX"             `shouldBe` "abcd"
-  it "e10" $
-    eliminaAisladas 'X' "abcdXX"            `shouldBe` "abcdXX"
-  it "e11" $
-    eliminaAisladas 'X' "abcdXXX"           `shouldBe` "abcdXXX"
-  it "e12" $
-    eliminaAisladas 'X' "abXcd"             `shouldBe` "abcd"
-  it "e13" $
-    eliminaAisladas 'X' "abXXcd"            `shouldBe` "abXXcd"
-  it "e14" $
-    eliminaAisladas 'X' "abXXXcd"           `shouldBe` "abXXXcd"
-  it "e15" $
-    eliminaAisladas 'X' "XabXcdX"           `shouldBe` "abcd"
-  it "e16" $
-    eliminaAisladas 'X' "XXabXXcdXX"        `shouldBe` "XXabXXcdXX"
-  it "e17" $
-    eliminaAisladas 'X' "XXXabXXXcdXXX"     `shouldBe` "XXXabXXXcdXXX"
-  it "e18" $
-    eliminaAisladas 'X' "XabXXcdXeXXXfXx"   `shouldBe` "abXXcdeXXXfx"
-
-spec :: Spec
-spec = do
-  describe "def. 1" $ specG eliminaAisladas1
-  describe "def. 2" $ specG eliminaAisladas2
-  describe "def. 3" $ specG eliminaAisladas3
-  describe "def. 4" $ specG eliminaAisladas4
-  describe "equivalencia" $ it "p1" $ property prop_eliminaAisladas
