8594094d44d9219c2fafaca7d07fbd75f037ab0d
Jose A. Alonso <JoseA.Alonso@gmail.com>
2026-02-13 13:24:07 +0100

    Añade: Sumas de divisores propios.

--- src/Sumas_de_divisores_propios.hs
+++ src/Sumas_de_divisores_propios.hs
@@ -1,12 +1,12 @@
 -- Sumas_de_divisores_propios.hs
 -- Sumas de divisores propios.
 -- José A. Alonso Jiménez <https://jaalonso.github.io>
--- Sevilla, 20-mayo-2022
+-- Sevilla, 18-Febrero-2015 (actualizado 13-Febrero-2026)
 -- ---------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------
 -- Definir la función
---    sumaDivisoresHasta :: Integer -> [(Integer,Integer)]
+--    sumaDivisoresHasta :: Int -> [(Int,Int)]
 -- tal que (sumaDivisoresHasta n) es la lista de los pares (a,b) tales
 -- que a es un número entre 1 y n y b es la suma de los divisores
 -- propios de a. Por ejemplo,
@@ -23,45 +23,62 @@ module Sumas_de_divisores_propios where
 import Data.Array (accumArray, assocs)
 import Data.List (genericLength, group)
 import Data.Numbers.Primes (primeFactors)
+import Math.NumberTheory.ArithmeticFunctions (sigma)
+import Test.Hspec (Spec, describe, hspec, it, shouldBe)
 import Test.QuickCheck
 
--- 1ª solución
--- ===========
+-- 1ª solución: Fuerza bruta
+-- =========================
 
-sumaDivisoresHasta1 :: Integer -> [(Integer,Integer)]
-sumaDivisoresHasta1 n = [(x, sum (divisores x)) | x <- [1..n]]
+sumaDivisoresHasta1 :: Int -> [(Int,Int)]
+sumaDivisoresHasta1 n =
+  [(x, sum (divisores x)) | x <- [1..n]]
 
-divisores :: Integer -> [Integer]
-divisores n = [x | x <- [1..n `div` 2], n `mod` x == 0]
+divisores :: Int -> [Int]
+divisores n =
+  [x | x <- [1..n `div` 2], n `mod` x == 0]
 
--- 2ª solución
--- ===========
+-- 2ª solución: Basada en factorización
+-- ====================================
 
-sumaDivisoresHasta2 :: Integer -> [(Integer,Integer)]
-sumaDivisoresHasta2 n = [(x, sumaDivisores x) | x <- [1..n]]
+sumaDivisoresHasta2 :: Int -> [(Int,Int)]
+sumaDivisoresHasta2 n =
+  [(x, sumaDivisores x) | x <- [1..n]]
 
-sumaDivisores :: Integer -> Integer
+sumaDivisores :: Int -> Int
 sumaDivisores x =
   product [(p^(e+1)-1) `div` (p-1) | (p,e) <- factorizacion x] - x
 
 -- (factorizacion x) es la lista de las bases y exponentes de la
 -- descomposición prima de x. Por ejemplo,
 --    factorizacion 600  ==  [(2,3),(3,1),(5,2)]
-factorizacion :: Integer -> [(Integer,Integer)]
-factorizacion = map primeroYlongitud . group . primeFactors
+factorizacion :: Int -> [(Int,Int)]
+factorizacion =
+  map primeroYlongitud . group . primeFactors
 
 -- (primeroYlongitud xs) es el par formado por el primer elemento de xs
 -- y la longitud de xs. Por ejemplo,
 --    primeroYlongitud [3,2,5,7] == (3,4)
-primeroYlongitud :: [a] -> (a,Integer)
+primeroYlongitud :: [a] -> (a,Int)
 primeroYlongitud (x:xs) =
   (x, 1 + genericLength xs)
 
--- 3ª solución
--- ===========
+-- 3ª solución: Usando sigma
+-- =========================
 
-sumaDivisoresHasta3 :: Integer -> [(Integer,Integer)]
-sumaDivisoresHasta3 n = assocs (accumArray (+) 0 (1,n) (divisoresHasta n))
+sumaDivisoresHasta3 :: Int -> [(Int,Int)]
+sumaDivisoresHasta3 n =
+  [(x, sumaDivisores3 x) | x <- [1..n]]
+
+sumaDivisores3 :: Int -> Int
+sumaDivisores3 x = sigma 1 x - x
+
+-- 4ª solución: Criba con accumArray
+-- =================================
+
+sumaDivisoresHasta4 :: Int -> [(Int,Int)]
+sumaDivisoresHasta4 n =
+  assocs (accumArray (+) 0 (1,n) (divisoresHasta n))
 
 -- (divisoresHasta n) es la lista de los pares (a,b) tales que a es
 -- un número entre 2 y n y b es un divisor propio e x. Por ejemplo,
@@ -69,18 +86,43 @@ sumaDivisoresHasta3 n = assocs (accumArray (+) 0 (1,n) (divisoresHasta n))
 --    [(2,1),(3,1),(4,1),(5,1),(6,1),(4,2),(6,2),(6,3)]
 --    λ> divisoresHasta 8
 --    [(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(4,2),(6,2),(8,2),(6,3),(8,4)]
-divisoresHasta :: Integer -> [(Integer,Integer)]
-divisoresHasta n = [(a,b) | b <- [1..n `div` 2], a <- [b*2, b*3..n]]
+divisoresHasta :: Int -> [(Int,Int)]
+divisoresHasta n =
+  [(a,b) | b <- [1..n `div` 2], a <- [b*2, b*3..n]]
+
+-- Verificación
+-- ============
+
+verifica :: IO ()
+verifica = hspec spec
+
+specG :: (Int -> [(Int,Int)]) -> Spec
+specG sumaDivisoresHasta = do
+  it "e1" $
+    sumaDivisoresHasta 12 `shouldBe`
+    [(1,0),(2,1),(3,1),(4,3),(5,1),(6,6),(7,1),(8,7),(9,4),(10,8),(11,1),(12,16)]
+
+spec :: Spec
+spec = do
+  describe "def. 1" $ specG sumaDivisoresHasta1
+  describe "def. 2" $ specG sumaDivisoresHasta2
+  describe "def. 3" $ specG sumaDivisoresHasta3
+  describe "def. 4" $ specG sumaDivisoresHasta4
+
+-- La verificación es
+--    λ> verifica
+--    4 examples, 0 failures
 
 -- Comprobación de equivalencia
 -- ============================
 
 -- La propiedad es
-prop_sumaDivisoresHasta :: Positive Integer -> Bool
+prop_sumaDivisoresHasta :: Positive Int -> Bool
 prop_sumaDivisoresHasta (Positive n) =
   all (== sumaDivisoresHasta1 n)
       [ sumaDivisoresHasta2 n
       , sumaDivisoresHasta3 n
+      , sumaDivisoresHasta4 n
       ]
 
 -- La comprobación es
@@ -91,19 +133,40 @@ prop_sumaDivisoresHasta (Positive n) =
 -- =========================
 
 -- La comparación es
---    λ> last (sumaDivisoresHasta1 (10^6))
---    (1000000,1480437)
---    (0.47 secs, 308,542,392 bytes)
---    λ> last (sumaDivisoresHasta2 (10^6))
---    (1000000,2480437)
---    (0.26 secs, 208,548,944 bytes)
---    λ> last (sumaDivisoresHasta3 (10^6))
---    (1000000,1480437)
---    (6.65 secs, 3,249,831,856 bytes)
+--    λ> maximum (map snd (sumaDivisoresHasta1 5000))
+--    11700
+--    (2.41 secs, 1,256,416,456 bytes)
+--    λ> maximum (map snd (sumaDivisoresHasta2 5000))
+--    11700
+--    (0.09 secs, 101,568,864 bytes)
+--    λ> maximum (map snd (sumaDivisoresHasta3 5000))
+--    11700
+--    (0.02 secs, 16,444,288 bytes)
+--    λ> maximum (map snd (sumaDivisoresHasta4 5000))
+--    11700
+--    (0.04 secs, 10,616,024 bytes)
 --
---    λ> last (sumaDivisoresHasta1 (5*10^6))
---    (5000000,7402312)
---    (2.27 secs, 1,540,543,352 bytes)
---    λ> last (sumaDivisoresHasta2 (5*10^6))
---    (5000000,12402312)
---    (1.19 secs, 1,040,549,800 bytes)
+--    λ> maximum (map snd (sumaDivisoresHasta2 100000))
+--    304920
+--    (2.14 secs, 4,485,878,400 bytes)
+--    λ> maximum (map snd (sumaDivisoresHasta3 100000))
+--    304920
+--    (0.19 secs, 343,279,536 bytes)
+--    λ> maximum (map snd (sumaDivisoresHasta4 100000))
+--    304920
+--    (0.37 secs, 262,899,320 bytes)
+--
+-- La comparación de (maximum (map snd (f 5000))) es:
+--    | Solución | Tiempo | Memoria   |
+--    |----------|--------|-----------|
+--    | Def. 1   | 2.41 s | 1256.4 MB |
+--    | Def. 2   | 0.09 s |  101.6 MB |
+--    | Def. 3   | 0.02 s |   16.4 MB |
+--    | Def. 4   | 0.04 s |   10.6 MB |
+
+-- La comparación de (maximum (map snd (f 100000))) es:
+--    | Solución | Tiempo | Memoria |
+--    |----------|--------|---------|
+--    | Def. 2   | 2.14 s | 4.48 GB |
+--    | Def. 3   | 0.19 s | 0.34 GB |
+--    | Def. 4   | 0.37 s | 0.26 GB |
